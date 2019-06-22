73e22ca59aa1648ff7d252d2c62d918435356862
Drakovek <DrakovekMail@gmail.com>
2019-06-22 15:15:24 -0700

    Added 'Start' class with 'main' method, which starts the program.

---
+++ MediaHoarder/src/drakovek/hoarder/file/Start.java
@@ -0,0 +1,23 @@
+package drakovek.hoarder.file;
+
+/**
+ * Main class for starting the Media Hoarder Program
+ * 
+ * @author Drakovek
+ * @version 2.0
+ * @since 2.0
+ */
+public class Start 
+{
+	/**
+	 * Starts the Media Hoarder Program
+	 * 
+	 * @param args Not Used
+	 * @since 2.0
+	 */
+	public static void main(String[] args)
+	{
+		
+	}//METHOD main(String[] args)
+	
+}//CLASS Start
