8e2e78bcd4ce35459ea08ee58d14b7e7c31b025b
Drakovek <DrakovekMail@gmail.com>
2019-06-22 17:20:35 -0700

    Added 'modeFrame' JFrame to open with 'ModeContainerGUI' constructor.

--- MediaHoarder/src/drakovek/hoarder/gui/modes/ModeContainerGUI.java
+++ MediaHoarder/src/drakovek/hoarder/gui/modes/ModeContainerGUI.java
@@ -1,6 +1,31 @@
 package drakovek.hoarder.gui.modes;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+/**
+ * Contains methods for creating the main GUI to switch between operation modes.
+ * 
+ * @author Drakovek
+ * @version 2.0
+ * @since 2.0
+ */
 public class ModeContainerGUI
 {
+	/**
+	 * Initializes the ModeContainerGUI class
+	 * 
+	 * @since 2.0
+	 */
+	public ModeContainerGUI()
+	{
+		JFrame modeFrame = new JFrame("Media Hoarder");
+		
+		modeFrame.pack();
+		modeFrame.setSize(new Dimension(100, 100));
+		modeFrame.setVisible(true);
 
+	}//CONSTRUCTOR
+	
 }//CLASS
