b9aa0b3e21e64a328a3b6df180d23dc6cc36ac27
Drakovek <DrakovekMail@gmail.com>
2019-06-24 16:56:51 +0000

    Created main mode GUI - Added to center of Frame for ModeContainerGUI

---
+++ MediaHoarder/src/drakovek/hoarder/gui/modes/ModesGUI.java
@@ -0,0 +1,32 @@
+package drakovek.hoarder.gui.modes;
+
+import drakovek.hoarder.file.DSettings;
+
+/**
+ * The starting ModeGUI for choosing between broad operations categories
+ * 
+ * @author Drakovek
+ * @version 2.0
+ * @since 2.0
+ */
+public class ModesGUI extends ModeBaseGUI
+{
+	/**
+	 * Initializes the ModesGUI object.
+	 * 
+	 * @param settings Program Settings
+	 * @since 2.0
+	 */
+	public ModesGUI(DSettings settings)
+	{
+		super(settings);
+		
+	}//CONSTRUCTOR
+
+	@Override
+	public void event(String id, int value)
+	{
+
+	}//METHOD
+	
+}//CLASS
