68c958c0e5866b40ce47b078fdaaf47bf5f77808
Drakovek <DrakovekMail@gmail.com>
2019-06-24 19:07:51 +0000

    Created DRunnable Object for dealing with background processes

---
+++ MediaHoarder/src/drakovek/hoarder/work/DRunnable.java
@@ -0,0 +1,46 @@
+package drakovek.hoarder.work;
+
+/**
+ * Default Runnable object for the program.
+ * 
+ * @author Drakovek
+ * @version 2.0
+ * @since 2.0
+ */
+public class DRunnable implements Runnable
+{
+	/**
+	 * ID of the work type
+	 * 
+	 * @since 2.0
+	 */
+	private String id;
+	
+	/**
+	 * Object implementing Worker interface. Called when DRunnable runs
+	 * 
+	 * @since 2.0
+	 */
+	private Worker worker;
+	
+	/**
+	 * Initializes the DRunnable object.
+	 * 
+	 * @param worker Object implementing Worker interface. Called when DRunnable runs
+	 * @param id ID of the work type
+	 */
+	public DRunnable(Worker worker, final String id)
+	{
+		this.id = id;
+		this.worker = worker;
+		
+	}//CONSTRUCTOR
+	
+	@Override
+	public void run()
+	{
+		worker.run(id);
+		
+	}//METHOD
+
+}//CLASS
---
+++ MediaHoarder/src/drakovek/hoarder/work/Worker.java
@@ -0,0 +1,20 @@
+package drakovek.hoarder.work;
+
+/**
+ * Interface to call for various working objects such as Runnable or SwingWorker
+ * 
+ * @author Drakovek
+ * @version 2.0
+ * @since 2.0
+ */
+public interface Worker
+{
+	/**
+	 * Called when a worker starts.
+	 * 
+	 * @param id ID of the work.
+	 * @since 2.0
+	 */
+	public void run(final String id);
+	
+}//CLASS
